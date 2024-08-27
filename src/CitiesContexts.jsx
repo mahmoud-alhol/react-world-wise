5670906fbd29d3e393bfe00c48fb762d6174e645
mahmoud-alhol <mahmoud.asem.w@gmail.com>
2024-08-27 14:42:37 +0300

    a page for city details with usecontext

--- src/CitiesContexts.jsx
+++ src/CitiesContexts.jsx
@@ -10,6 +10,8 @@ const CitiesContexts = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+
   useEffect(function () {
     async function fetchCities() {
       try {
@@ -25,8 +27,24 @@ function CitiesProvider({ children }) {
     }
     fetchCities();
   }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <CitiesContexts.Provider value={{ cities, isLoading }}>{children}</CitiesContexts.Provider>
+    <CitiesContexts.Provider value={{ cities, isLoading, currentCity, getCity }}>
+      {children}
+    </CitiesContexts.Provider>
   );
 }
 
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCities } from "../CitiesContexts";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -10,10 +11,14 @@ const formatDate = (date) =>
 
 function CityItem({ city }) {
   const { cityName, emoji, date, id, position } = city;
+  const { currentCity } = useCities();
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+      <Link
+        className={`${styles.cityItem} ${id === currentCity.id ? styles["cityItem--active"] : ""}`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji} </span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
